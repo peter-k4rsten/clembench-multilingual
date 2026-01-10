efe6a1ebd0618d37abbd4d1537a6e1ff4c3c9c2f
Eszter Bukovszky <eszter.bukovszky@uni-potsdam.de>
2026-01-10 10:31:39 +0100

    localization utils for image game

---
+++ imagegame/resources/localization_utils.py
@@ -0,0 +1,14 @@
+MULTILINGUAL_PATTERNS = {
+    "en": {
+        "command_tag": "command:",
+        "done_word": "done",
+        "grid_word": "grid",
+        "dimension_format": "{n} by {n}"
+    },
+    "hu": {
+        "command_tag": "parancs:",
+        "done_word": "kész",
+        "grid_word": "rács",
+        "dimension_format": "{n}x{n}"
+    }
+}
